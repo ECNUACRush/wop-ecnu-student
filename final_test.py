02d4735e67841508d89d2966a29faa87c39bced0
ECNUACRush <www.xiaoyuya@qq.com>
2020-03-09 12:30:30 +0800

    ok it's morning first test

---
+++ final_test.py
@@ -0,0 +1,9 @@
+def finalTest():
+    print('haha, it\'s final test')
+
+
+py = {'1': 'python', 2: 'git', 3: 'beautiful', 'date': 10.8}
+for i in py:
+    print('i is ', i)
+    print('value is ', py[i])
+finalTest()
